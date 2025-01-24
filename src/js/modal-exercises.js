3f31d86ad20d7bc6ae86e88be3ec6413201ef2e8
Vladyslav Kovalchuk <vladkovalchuk09@gmail.com>
2025-01-24 18:47:35 +0200

    rewrite as promise

--- src/js/modal-exercises.js
+++ src/js/modal-exercises.js
@@ -4,7 +4,6 @@ const modalpage= document.querySelector('.modal-page');
 
 if (modalpage) 
 {
-    const modalExercises = await Exercise.fetchById("64f389465ae26083f39b17df");
-    modalpage.append(modalExercises.renderCard());
+    Exercise.fetchById("64f389465ae26083f39b17df").then((exercise) => {  modalpage.append(exercise.renderCard()); });
 }
     
