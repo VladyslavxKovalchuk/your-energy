e96dc7e22bdf793cd4863910e76efd4ee39f983f
Vladyslav Kovalchuk <vladkovalchuk09@gmail.com>
2025-01-24 18:31:57 +0200

    update

--- src/js/modal-exercises.js
+++ src/js/modal-exercises.js
@@ -1,18 +1,10 @@
-import Exercise from './exercise.Class';
+import Exercise from "./exercise.Class";
 
-const closebtn = document.querySelector('.const modal-card-close-button');
-const modalpage = document.querySelector('.modal-page');
-if (closebtn){
-  closebtn.addEventListener('click', () => {
-    if (modalpage){
-        modalpage.classList.add('hidden');
-        modalpage.innerHTML = '';
-    }
-  });
+const modalpage= document.querySelector('.modal-page');
+
+if (modalpage) 
+{
+    const modalExercises = await Exercise.fetchById("64f389465ae26083f39b17df");
+    modalpage.append(modalExercises.renderCard());
 }
-
-
-if (modalpage) {
-  const modalExercises = await Exercise.fetchById('64f389465ae26083f39b17df');
-  modalpage.append(modalExercises.renderCard());
-}
+    
